486c42109714ff094e3a879b6c4950b0b95c0740
Mike Kazantsev <mk.fraggod@gmail.com>
2013-07-06 19:15:48 +0600

    nfct_logger: implemented parser for event data

--- nfct_logger.py
+++ nfct_logger.py
@@ -3,9 +3,43 @@
 from __future__ import print_function
 
 import itertools as it, operator as op, functools as ft
+from xml.etree import ElementTree
+from io import BytesIO
+from datetime import datetime
+from collections import namedtuple
 import os, sys, logging
 
 from nfct_cffi import NFCT
+
+
+FlowData = namedtuple('FlowData', 'ts proto src dst sport dport')
+
+def parse_event(ev_xml):
+	etree = ElementTree.parse(BytesIO(ev_xml))
+
+	flow = next(etree.iter())
+	assert flow.attrib['type'] == 'new', ev_xml
+
+	ts = flow.find('when')
+	ts = datetime(*(int(ts.find(k).text) for k in ['year', 'month', 'day', 'hour', 'min', 'sec']))
+
+	flow_data = dict()
+	for meta in flow.findall('meta'):
+		if meta.attrib['direction'] in ['original', 'reply']:
+			l3, l4 = it.imap(meta.find, ['layer3', 'layer4'])
+			proto = '{}/{}'.format(l3.attrib['protoname'], l4.attrib['protoname'])
+			src, dst = (l3.find(k).text for k in ['src', 'dst'])
+			sport, dport = (l4.find(k).text for k in ['sport', 'dport'])
+			flow_data[meta.attrib['direction']] = FlowData(ts, proto, src, dst, sport, dport)
+
+	# Fairly sure all new flows have to be symmetrical, check that
+	fo, fr = op.itemgetter('original', 'reply')(flow_data)
+	assert fo.proto == fr.proto\
+		and fo.src == fr.dst and fo.dst == fr.src\
+		and fo.sport == fr.dport and fo.dport == fr.sport,\
+		flow_data
+
+	return flow_data['original']
 
 
 def main(argv=None):
@@ -16,13 +50,17 @@ def main(argv=None):
 	opts = parser.parse_args(argv or sys.argv[1:])
 
 	import logging
-	logging.basicConfig(level=logging.DEBUG if opts.debug else logging.WARNING)
+	logging.basicConfig(level=logging.DEBUG if opts.debug else logging.INFO)
 	log = logging.getLogger()
 
-	src = NFCT().generator()
+	nfct = NFCT()
+	src = nfct.generator(events=nfct.libnfct.NFNLGRP_CONNTRACK_NEW)
 	next(src) # fd
-	for data in src:
-		log.debug('Event: {}'.format(data))
+
+	for ev_xml in src:
+		# log.debug('Event: {}'.format(ev_xml))
+		flow = parse_event(ev_xml)
+		log.info('{0.proto} {0.src}/{0.sport} > {0.dst}/{0.dport}'.format(flow))
 
 
 if __name__ == '__main__': sys.exit(main())
