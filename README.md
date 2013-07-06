432cdfb876460f2d6cecdbaccc08534d53de2ad0
Mike Kazantsev <mk.fraggod@gmail.com>
2013-07-06 22:38:27 +0600

    nfct_logger: added fetching pid information

--- README.md
+++ README.md
@@ -19,12 +19,43 @@ This tool allows to attribute such logged connections to pids (which might be
 e.g. forked curl, hence not useful by itself) and services they belong to,
 assuming proper service-pid-tracking system (i.e. systemd cgroups) is in place.
 
+Unlike e.g. [netstat-monitor](https://github.com/stalexan/netstat-monitor/), it
+doesn't poll `/proc/net/*` paths, getting "new flow" events via
+libnetfilter_conntrack (netlink socket) instead, so should catch even the most
+transient flows and not hog any extra locks on polling.
+
 
 
 Usage
 --------------------
 
-Soon.
+Just run nfct_logger.py and get the entries from its stdout (lines wrapped for readability):
+
+```console
+# ./nfct_logger.py -p tcp
+1373127181: ipv6/tcp 2001:470:1f0b:11de::12/55446 > 2607:f8b0:4006:802::1010/80 ::
+	2354 1000:1000 /user/1000.user/1.session/systemd-1196/enlightenment.service ::
+	curl -s -o /dev/null ipv6.google.com
+1373127199: ipv4/tcp 192.168.0.12/34870 > 195.24.232.208/80 ::
+	28865 1000:1000 /user/1000.user/1.session/systemd-1196/dbus.service ::
+	python /usr/libexec/dbus-lastfm-scrobbler
+1373127220: ipv4/tcp 127.0.0.1/59047 > 127.0.0.1/1234 ::
+	2387 1000:1000 /user/1000.user/1.session/systemd-1196/enlightenment.service ::
+	ncat -v cane 1234
+```
+
+Default log format (can be controlled via --format, timestamp format via --format-ts) is (wrapped):
+
+	{ts}: {ev.proto} {ev.src}/{ev.sport} > {ev.dst}/{ev.dport} ::
+		{info.pid} {info.uid}:{info.gid} {info.service} :: {info.cmdline}
+
+Info about pid might not be available for transient connections, like one-way
+udp packets, as these don't seem to end up in /proc/net/udp (or udp6) tables,
+hence it's hard to get socket "inode" number.
+
+As netfilter, conntrack and netlink sockets are linux-specific things (afaik),
+script should not work on any other platforms, unless there is some
+compatibility layer in place.
 
 
 ### nfct_cffi
@@ -42,3 +73,37 @@ for data in src:
 ```
 
 Module uses libnetfilter_conntrack via CFFI.
+
+
+
+Installation
+--------------------
+
+It's a regular package for Python 2.7 (not 3.X), but not in pypi, so can be
+installed from a checkout with something like that:
+
+	% python setup.py install
+
+Better way would be to use [pip](http://pip-installer.org/) to install all the
+necessary dependencies as well:
+
+	% pip install 'git+https://github.com/mk-fg/conntrack-logger.git#egg=conntrack-logger'
+
+Note that to install stuff in system-wide PATH and site-packages, elevated
+privileges are often required.
+Use "install --user",
+[~/.pydistutils.cfg](http://docs.python.org/install/index.html#distutils-configuration-files)
+or [virtualenv](http://pypi.python.org/pypi/virtualenv) to do unprivileged
+installs into custom paths.
+
+Alternatively, `./nfct_logger.py` can be run right from the checkout tree
+without any installation.
+
+### Requirements
+
+* Python 2.7 (not 3.X)
+* [libnetfilter_conntrack](http://www.netfilter.org/projects/libnetfilter_conntrack)
+* [CFFI](http://cffi.readthedocs.org) (for libnetfilter_log bindings)
+
+CFFI uses C compiler to generate bindings, so gcc (or other compiler) should be
+available if module is being built from source or used from checkout tree.
--- nfct_logger.py
+++ nfct_logger.py
@@ -6,8 +6,8 @@ import itertools as it, operator as op, functools as ft
 from xml.etree import ElementTree
 from io import BytesIO
 from datetime import datetime
-from collections import namedtuple
-import os, sys, logging
+from collections import namedtuple, defaultdict
+import os, sys, logging, re, glob, errno, socket
 
 from nfct_cffi import NFCT
 
@@ -27,9 +27,11 @@ def parse_event(ev_xml):
 	for meta in flow.findall('meta'):
 		if meta.attrib['direction'] in ['original', 'reply']:
 			l3, l4 = it.imap(meta.find, ['layer3', 'layer4'])
-			proto = '{}/{}'.format(l3.attrib['protoname'], l4.attrib['protoname'])
+			proto = l3.attrib['protoname'], l4.attrib['protoname']
+			if proto[1] not in ['tcp', 'udp']: return
+			proto = '{}/{}'.format(*proto)
 			src, dst = (l3.find(k).text for k in ['src', 'dst'])
-			sport, dport = (l4.find(k).text for k in ['sport', 'dport'])
+			sport, dport = (int(l4.find(k).text) for k in ['sport', 'dport'])
 			flow_data[meta.attrib['direction']] = FlowData(ts, proto, src, dst, sport, dport)
 
 	# Fairly sure all new flows should be symmetrical, check that
@@ -42,20 +44,120 @@ def parse_event(ev_xml):
 	return flow_data['original']
 
 
+def parse_ipv4(enc):
+	return socket.inet_ntop(socket.AF_INET, ''.join(reversed(enc.decode('hex'))))
+
+def parse_ipv6( enc,
+		_endian=op.itemgetter(*(slice(n*2, (n+1)*2) for n in [6, 7, 4, 5, 2, 3, 0, 1])) ):
+	return socket.inet_ntop( socket.AF_INET6,
+		''.join(_endian(''.join(reversed(enc.decode('hex'))))) )
+
+def get_table_sk( proto,
+		_line_proc=op.itemgetter(1, 2, 9),
+		_proto_conns={
+			'ipv4/tcp': '/proc/net/tcp', 'ipv6/tcp': '/proc/net/tcp6',
+			'ipv4/udp': '/proc/net/udp', 'ipv6/udp': '/proc/net/udp6' } ):
+	_ntoa = ft.partial(parse_ipv4 if proto.startswith('ipv4/') else parse_ipv6)
+	with open(_proto_conns[proto]) as src:
+		next(src)
+		for line in src:
+			a, b, sk = _line_proc(line.split())
+			k = (ep.split(':', 1) for ep in [a, b])
+			k = tuple(sorted((_ntoa(ip), int(p, 16)) for ip,p in k))
+			yield k, sk
+
+
+def get_table_links():
+	links = list()
+	for path in glob.iglob('/proc/[0-9]*/fd/[0-9]*'):
+		try: link = os.readlink(path)
+		except OSError as err:
+			if err.errno != errno.ENOENT: raise
+			continue
+		links.append((path, link))
+	for path, link in links:
+		match = re.search(r'^socket:\[([^]]+)\]$', link)
+		if not match: continue
+		yield match.group(1), int(re.search(r'^/proc/(\d+)/', path).group(1))
+
+
+def pid_info(pid, entry):
+	return open('/proc/{}/{}'.format(pid, entry)).read()
+
+class FlowInfo(namedtuple('FlowInfo', 'pid uid gid cmdline service')):
+	__slots__ = tuple()
+
+	def __new__(cls, pid=None):
+		uid = gid = cmdline = service = '-'
+		if pid is not None:
+			try:
+				cmdline, service = (pid_info(pid, k) for k in ['cmdline', 'cgroup'])
+				stat = os.stat('/proc/{}'.format(pid))
+				uid, gid = op.attrgetter('st_uid', 'st_gid')(stat)
+			except OSError:
+				if err.errno != errno.ENOENT: raise
+			if cmdline != '-': cmdline = cmdline.replace('\0', ' ').strip()
+			if service != '-':
+				for line in service.splitlines():
+					line = line.split(':')
+					if not re.search(r'^name=', line[1]): continue
+					service = line[2]
+					break
+		return super(FlowInfo, cls).__new__(cls, pid or '?', uid, gid, cmdline, service)
+
+
+def get_flow_info(flow, _nx=FlowInfo(), _cache=dict()):
+	_cache = _cache.setdefault(flow.proto, defaultdict(dict))
+
+	cache = _cache['sk']
+	ip_key = tuple(sorted([(flow.src, flow.sport), (flow.dst, flow.dport)]))
+	if ip_key not in cache:
+		cache.clear()
+		cache.update(get_table_sk(flow.proto))
+	if ip_key not in cache:
+		log.info('Failed to find connection for {}'.format(ip_key))
+		return _nx
+	sk = cache[ip_key]
+
+	cache = _cache['links']
+	if sk not in cache:
+		cache.clear()
+		cache.update(get_table_links())
+	if sk not in cache:
+		log.info('Failed to find pid for {}'.format(ip_key))
+		return _nx
+	pid = cache[sk]
+
+	cache = _cache['info']
+	pid_ts = int(pid_info(pid, 'stat').split()[21])
+	if pid in cache:
+		info_ts, info = cache[pid]
+		if pid_ts != info_ts: del cache[pid] # check starttime to detect pid rotation
+	if pid not in cache:
+		cache[pid] = pid_ts, FlowInfo(pid)
+	info_ts, info = cache[pid]
+
+	return info
+
+
 def main(argv=None):
 	import argparse
 	parser = argparse.ArgumentParser(description='conntrack event logging/audit tool.')
-	parser.add_argument('-t', '--ts-format', default='%s',
-		help='Timestamp format, as for datetime.strftime() (default: %(defauls)s).')
+	parser.add_argument('-p', '--protocol',
+		help='Regexp (python) filter to match "ev.proto". Examples: ipv4, tcp, ipv6/udp.')
+	parser.add_argument('-t', '--format-ts', default='%s',
+		help='Timestamp format, as for datetime.strftime() (default: %(default)s).')
 	parser.add_argument('-f', '--format',
-		default='{ts}: {ev.proto} {ev.src}/{ev.sport} > {ev.dst}/{ev.dport}',
-		help='Output format for each new flow, as for str.format() (default: %(defauls)s).')
+		default='{ts}: {ev.proto} {ev.src}/{ev.sport} > {ev.dst}/{ev.dport}'
+			' :: {info.pid} {info.uid}:{info.gid} {info.service} :: {info.cmdline}',
+		help='Output format for each new flow, as for str.format() (default: %(default)s).')
 	parser.add_argument('--debug',
 		action='store_true', help='Verbose operation mode.')
 	opts = parser.parse_args(argv or sys.argv[1:])
 
 	import logging
-	logging.basicConfig(level=logging.DEBUG if opts.debug else logging.INFO)
+	logging.basicConfig(level=logging.DEBUG if opts.debug else logging.WARNING)
+	global log
 	log = logging.getLogger()
 
 	nfct = NFCT()
@@ -63,8 +165,14 @@ def main(argv=None):
 	next(src) # netlink fd
 
 	for ev_xml in src:
-		ev = parse_event(ev_xml)
-		print(opts.format.format(ev=ev, ts=ev.ts.strftime(opts.ts_format)))
+		try: ev = parse_event(ev_xml)
+		except:
+			log.error('Failed to parse event data: {}'.format(ev_xml))
+			continue
+		if not ev: continue
+		if opts.protocol and not re.search(opts.protocol, ev.proto): continue
+		print(opts.format.format( ev=ev,
+			ts=ev.ts.strftime(opts.format_ts), info=get_flow_info(ev) ))
 
 
 if __name__ == '__main__': sys.exit(main())
